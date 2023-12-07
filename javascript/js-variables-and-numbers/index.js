36e6d901cbf79b8e9499f908dedf597ed1f0842d
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-07 11:56:16 +0100

    javascript notes

---
+++ javascript/js-variables-and-numbers/index.js
@@ -0,0 +1,34 @@
+// This is a comment
+
+//depricated
+var oldUse;
+oldUse = "Try not to use this";
+
+console.log("old Use:", oldUse);
+
+/* Ways to run JS files:
+//1. In the terminal: Stand in the folder of file and write in the terminal node fileName.js (eg index.js)
+
+2. In VS code, with Run without Debugging  (select node)
+
+3. In the browser. 
+
+from 2015: */
+
+let newVariable;
+
+const myValueWillNotChange = "You are stuck with me!";
+
+console.log("newVariable: ", newVariable);
+console.log("myValueWillNotChange: ", myValueWillNotChange);
+
+const myBigInt = 3000n;
+console.log("myBigInt: ", myBigInt);
+
+// null
+const myNull = null;
+console.log("myNull: ", myNull);
+
+// Boleans
+
+// Let's do math
---
+++ javascript/js-variables-and-numbers/js-variables-and-numbers.md
@@ -0,0 +1,39 @@
+## Data Types
+
+Primitive
+number
+boolean
+BigInt
+Symbol
+null
+Undefined
+
+## Reference
+
+## Math
+
+parseInt <-- converses into a number
+
+let amIaNumber = 2;
+
+amIaNumber += 1;
+
+amIaNumber++; <-- adds 1
+
+### substract
+
+converts string to number
+
+## -= 3
+
+### divided
+
+/=
+\*=
+
+Even or uneven
+Remainder
+%= 2
+
+potential = \*\*
+\*\*=
